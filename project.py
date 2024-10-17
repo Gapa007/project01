49edf5798d5a08149fd6b68b1d9e35ad5bc90544
Gapa007 <t.vasko@seznam.cz>
2024-10-17 18:26:17 +0200

    nothing added

--- project.py
+++ project.py
@@ -34,69 +34,80 @@ in modern oceans. Other fish such as paddlefish,
 garpike and stingray are also present.'''
 ] 
 
-# fixed input
-user_with_passwd = {
-    "bob": "123",
-    "ann": "pass123",
-    "mike": "password123",
-    "liz": "pass123"
-}
+import os
 
-# login
-user = input("username: ")
-passwd = input("password: ")
+user_data = {"bob" : "123","ann" : "pass123","mike" : "password123",
+             "liz" : "pass123"}
+separator = "=" * 40
 
-if user not in user_with_passwd or user_with_passwd[user] != passwd:
-    print("unregistered user, terminating the program..")
-    exit()
-else:
-    print(f"""
-----------------------------------------
-Welcome to the app, {user}
-We have 3 texts to be analyzed.
-----------------------------------------
-""")   
+while True:
+    if user_name := input("ENTER YOUR NAME: "):
+        break
+    print("EMPTY INPUT. TRY AGAIN....")
 
-#text analyse
-choice_text_nr = int(input("Enter a number btw 1 and 3 to select: ")) 
-print("----------------------------------------")
-
-if 1 <= choice_text_nr <= 3:
-    selected_text = TEXTS[choice_text_nr - 1]
-
-    words = selected_text.replace(",", " ").split()
+while True:   
+    if password := input("ENTER YOUR PASSWORD: "):
+        break
+    print("EMPTY INPUT. TRY AGAIN...")
     
-    count_words = len(words)  # 1
-    count_titled = sum(1 for word in words if word.istitle())  # 2
-    count_upper = sum(1 for word in words if word.isupper() and word.isalpha())  # 3
-    count_lower = sum(1 for word in words if word.islower())  # 4
-    count_numer_string = sum(1 for word in words if word.isdigit())  # 5
-    sum_of_numbers = sum(int(word) for word in words if word.isdigit())  # 6
+print(separator)
 
-    print(f"There are {count_words} words in the selected text.")  # 1
-    print(f"There are {count_titled} titlecase words.")  # 2
-    print(f"There are {count_upper} uppercase words.")  # 3
-    print(f"There are {count_lower} lowercase words.")  # 4
-    print(f"There are {count_numer_string} numeric strings.")  # 5
-    print(f"The sum of all the numbers is {sum_of_numbers}.")  # 6
+if password == str(user_data.get(user_name)):
+    print(f"WELCOME TO THE APP, {user_name.upper()}!")
+    print(f"WE HAVE {len(TEXTS)} TEXTS TO BE ANALYZED.")    
+    
+    print(separator)
+    
+else:
+    os.system("cls")
+    print("UNREGISTERED USER, TERMINATING THE PROGRAM..")
+    exit()
+         
+while True:   
+    choice = input(f"ENTER A NUMBER BTW. 1 AND {len(TEXTS)} TO SELECT:\n")
 
-print("----------------------------------------")
-print("LEN|  OCCURENCES  |NR.")
-print("----------------------------------------")
+    print(separator)
 
-length_occurrences = {}
-
-words = selected_text.replace(",", "").split()
-
-for word in words:
-    length = len(word) 
-    if length in length_occurrences:
-        length_occurrences[length] += 1 
+    if not choice.isnumeric() or int(choice) not in range(1, len(TEXTS) + 1):
+        os.system("cls") 
+        print("INVALID INPUT. CHOOSE ONLY NUMBERS BETWEEN 1-3")
+        exit()
+            
     else:
-        length_occurrences[length] = 1  
+        choice = TEXTS[int(choice) - 1]
+        break
 
-for length in range(1, max(length_occurrences.keys()) + 1):
-    occurrences = length_occurrences.get(length, 0)  
-    print(f"{length:<2} | {'*' * occurrences:<12} | {occurrences}")
+splited_text = choice.split()
+words = [word.strip(",.?!") for word in splited_text]
+numbers_of_length = [len(word) for word in words]
+title_case = [word for word in words if word.istitle()]
+all_upper = [word for word in words if word.isupper() and word.isalpha()]
+all_lower = [word for word in words if word.islower()]
+numeric_string = [word for word in words if word.isnumeric()]
+numeric_int = [int(number) for number in numeric_string]
 
-print("----------------------------------------")
+print(f"THERE ARE {len(numbers_of_length)} WORDS IN SELECTED TEXT.")
+print(f"THERE ARE {len(title_case)} TITLECASE WORDS.")
+print(f"THERE ARE {len(all_upper)} UPPERCASE WORDS.")
+print(f"THERE ARE {len(all_lower)} LOWERCASE WORDS.")
+print(f"THERE ARE {len(numeric_string)} NUMERIC STRINGS.")
+print(f"THE SUM OF ALL THE NUMBERS IS {sum(numeric_int)}.")
+print(separator)
+print("LEN| OCCURENCES| NR.")
+print(separator)
+
+star = "*"
+space = " "
+set_numbers = set(numbers_of_length)
+frequency = []
+
+for each in set_numbers:
+    frequency.append(numbers_of_length.count(each))
+highest_number = max(frequency)
+
+for num in set_numbers:
+    count_of_spaces = highest_number - numbers_of_length.count(num)
+    print(
+        f"{num:2d} | {star * numbers_of_length.count(num)}"
+        f"{space * count_of_spaces} | {numbers_of_length.count(num)}"
+    )
